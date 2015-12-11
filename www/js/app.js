58d67ba919b421c824be44558de52453b41f4003
estevanbenjamin <estevanbenjamin@gmail.com>
2015-12-10 17:57:17 -0800

    started working on that directive without making a new branch..bad boy

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('forms', ['ionic', 'firebase'])
+angular.module('forms', ['ionic', 'firebase', 'custom'])
   .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -31,6 +31,7 @@ angular.module('forms', ['ionic', 'firebase'])
 
 
 .controller('FormCtrl', function($scope, $stateParams, Stack, $firebaseObject) {
+  var ce = angular.element(document.querySelector('#contentelem'));
   $scope.formData={};
   var ref = new Firebase("https://sandboxforms.firebaseio.com/data/0/");
   var data = $firebaseObject(ref);
